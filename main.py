73ad1c0400a56eed05d019c4e5c34d5d39521d8a
kevas007 <kevassiobo@gmail.com>
2022-11-14 14:43:26 +0100

    add

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-nombre1 = int( input("Entrer le premier nombre :  "))
-nombre2 = int(input("Entrer le second nombre :  "))
+try:
+    nombre1 = int( input("Entrer le premier nombre :  "))
+    nombre2 = int(input("Entrer le second nombre :  "))
 
-op = input("Entrer l'opérateur: ")
+    op = input("Entrer l'opérateur: ")
 
-if op == "+" :
-    result= nombre1 + nombre2
-    print( result )
-elif op == "-":
-    result=  nombre1 -  nombre2
-    print( result )
-elif op == "*":
-    result=  nombre1 *  nombre2
-    print( result )
-elif op == "-":
-    result=  nombre1 /  nombre2
-    print( abs(result ))
-else:
-    print('Invalid opérateur')
+    if op == "+" :
+        result= nombre1 + nombre2
+        print( result )
+    elif op == "-":
+        result=  nombre1 -  nombre2
+        print( result )
+    elif op == "*":
+        result=  nombre1 *  nombre2
+        print( result )
+    elif op == "-":
+        result=  nombre1 /  nombre2
+        print( abs(result ))
+    else:
+        print('Invalid opérateur')
+except:
+    print('Somehting went rong')
