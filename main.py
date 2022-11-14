e69a2f342164dd3f36146b367399c0e53f939b94
kevas007 <kevassiobo@gmail.com>
2022-11-14 14:36:16 +0100

    base

---
+++ main.py
@@ -0,0 +1,19 @@
+nombre1 = int( input("Entrer le premier nombre :  "))
+nombre2 = int(input("Entrer le second nombre :  "))
+
+op = input("Entrer l'opérateur: ")
+
+if op == "+" :
+    result= nombre1 + nombre2
+    print( result )
+elif op == "-":
+    result=  nombre1 -  nombre2
+    print( result )
+elif op == "*":
+    result=  nombre1 *  nombre2
+    print( result )
+elif op == "-":
+    result=  nombre1 /  nombre2
+    print( abs(result ))
+else:
+    print('Invalid opérateur')
